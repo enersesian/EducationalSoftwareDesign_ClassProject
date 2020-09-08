0bda1bfb8fc05f153190d88e91c7cbae1c856049
enersesian <en6@njit.edu>
2020-09-07 20:53:53 -0400

    Built title scene UI and sceneManager singleton

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager sceneManagerSingleton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(sceneManagerSingleton == null)
+        {
+            sceneManagerSingleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    public void SetScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}

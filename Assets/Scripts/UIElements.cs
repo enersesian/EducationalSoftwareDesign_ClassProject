9ee84778c3f52519c82a70d1f4484b885fa8f9a5
enersesian <en6@njit.edu>
2020-10-25 21:15:48 -0400

    Adding Bubble Sort WebGL assets and built an UI experimental scene

---
+++ Assets/Scripts/UIElements.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElements : MonoBehaviour
+{
+    public Image panelImage;
+    public Slider blueSlider;
+
+    public void ButtonClicked()
+    {
+        panelImage.color = Color.red;
+        blueSlider.value = 0f;
+    }
+
+    public void TogglePanelVisible(bool newPanelVisbility)
+    {
+        panelImage.enabled = newPanelVisbility;
+    }
+
+    public void SetPanelBlueAmount(float newBlueAmount)
+    {
+        panelImage.color = new Color(panelImage.color.r, panelImage.color.g, newBlueAmount, panelImage.color.a);
+    }
+}
